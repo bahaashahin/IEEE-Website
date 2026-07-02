afa88b78ce38186895cf469ad95060cbdef4b56b
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-07-02 16:23:15 +0300

    feat: add shared TypeScript types for board members - Define BoardMember interface and BoardMemberType union type - Export from types barrel index.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,11 @@
+export { default as About } from "./About";
+export { default as Board } from "./Board";
+export { default as Committees } from "./Committees";
+export { default as ContactUs } from "./ContactUs";
+export { default as Dashboard } from "./Dashboard";
+export { default as EventDetails } from "./EventDetails";
+export { default as Events } from "./Events";
+export { default as FeedbackDashboard } from "./FeedbackDashboard";
+export { default as Home } from "./Home";
+export { default as JoinUs } from "./JoinUs";
+export { default as Login } from "./Login";
