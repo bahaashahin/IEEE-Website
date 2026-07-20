07e2ab1eea12f440afe817149349bc208e2be2f3
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-07-20 21:03:39 +0300

    fix/style: uppercase pr&fr and hr positions

--- src/utils/member.position.ts
+++ src/utils/member.position.ts
@@ -1,20 +1,20 @@
 // TODO: we should write down the type of the member here instead of relying on the type any or this hard coded object
 export const selectMemberPosition = (
-	member: { position: string; memberType: string; track: string },
-	chairGender: "male" | "female",
+  member: { position: string; memberType: string; track: string },
+  chairGender: "male" | "female",
 ) => {
-	let position: string;
-	if (member.position === "chair") {
-		position = chairGender === "female" ? "chair lady" : "chair man";
-	} else if (["vice branding", "vice technical"].includes(member.position)) {
-		position =
-			chairGender === "female"
-				? `vice chair lady ${member.position.split(" ")[1]}`
-				: `vice chair man ${member.position.split(" ")[1]}`;
-	} else if (member.memberType === "officer") {
-		position = member.position;
-	} else {
-		position = `${member.position} of ${member.track}`;
-	}
-	return position;
+  let position: string;
+  if (member.position === "chair") {
+    position = chairGender === "female" ? "chair lady" : "chair man";
+  } else if (["vice branding", "vice technical"].includes(member.position)) {
+    position =
+      chairGender === "female"
+        ? `vice chair lady ${member.position.split(" ")[1]}`
+        : `vice chair man ${member.position.split(" ")[1]}`;
+  } else if (member.memberType === "officer") {
+    position = member.position;
+  } else {
+    position = `${member.position} of ${member.track.includes("pr&fr") || member.track.includes("hr") ? member.track.toUpperCase() : member.track}`;
+  }
+  return position;
 };
